81e9269972914db799286170f99921ac813ade21
kinses38 <shaun.kinsella38@mail.dcu.ie>
2019-04-03 18:42:52 +0100

    Create generic simple room to test deploy

---
+++ src/main/java/application/Rooms.java
@@ -0,0 +1,15 @@
+package application;
+
+public class Rooms {
+
+    private String room = "L128";
+
+    public Rooms(){
+        this.room = room;
+    }
+
+    public String getRooms()
+    {
+        return room;
+    }
+}
