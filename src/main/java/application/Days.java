6d749d8c76583a4d7ae5f6ab2d1cc2d12a234674
kinses38 <shaun.kinsella38@mail.dcu.ie>
2019-04-03 20:56:00 +0100

    Add json reader, add representative classes for mapping json structs to java objects, change rest get to return array of all rooms

---
+++ src/main/java/application/Days.java
@@ -0,0 +1,26 @@
+package application;
+
+public class Days {
+
+    private String day;
+    private int [] timeslotCapacity;
+
+
+    public String getDay(){
+        return day;
+    }
+
+    public void setDay(){
+        this.day = day;
+    }
+
+    public int [] getTimeslotCapacity()
+    {
+        return timeslotCapacity;
+    }
+
+    public void setTimes()
+    {
+        this.timeslotCapacity = timeslotCapacity;
+    }
+}
