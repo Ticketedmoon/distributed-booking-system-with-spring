0d0806b67167027061e162d22b75c1e4416e0bc1
kinses38 <shaun.kinsella38@mail.dcu.ie2>
2019-04-04 22:13:55 +0100

    added booking functionality

--- src/main/java/application/Rooms.java
+++ src/main/java/application/Rooms.java
@@ -21,4 +21,11 @@ public class Rooms {
     public Object returnDay(String roomName, int dayOfWeek){
         return rooms.get(roomName).getDays()[dayOfWeek];
     }
+
+    public HashMap <String, Room> updateBooking(String roomName, int dayOfWeek, String timeslot)
+    {
+        HashMap<String, Room> updatedRooms = rooms;
+        updatedRooms.get(roomName).getDays()[dayOfWeek].updateTimeSlotCapacity(timeslot);
+        return updatedRooms;
+    }
 }
